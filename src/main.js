6059c42047ad5b357e095e311786c162bcae2867
Lucas Goudin <lucas@kasual.biz>
2020-12-20 15:08:31 +0100

    Dockerized frontend and added axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from "bootstrap-vue"
+import axios from 'axios';
 import App from './App.vue'
 import { router } from "@/router";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 Vue.use(VueRouter);
 
 new Vue({
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="container">
-    Home
+    <button @click="fetchMovies">Fetch movies</button>
+    <ul>
+      <li v-for="movie in movies" v-bind:key='movie.id'>
+        {{movie.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   export default {
-  name: "Home"
+  name: "Home",
+          data() {
+            return {
+              movies: [],
+            };
+          },
+          methods: {
+            fetchMovies() {
+              const baseURI = "http://localhost:3000/api/movies/all";
+              this.$http.get(baseURI).then(result => this.movies = result.data)
+            }
+          }
   }
 </script>
 
