0b62b979bc188f8ae555ae42a7dba79f6863d960
Lucas Goudin <goudinlucas@gmail.com>
2020-12-30 22:21:51 +0100

    Card details on hover

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,19 +1,18 @@
 <template>
-  <v-card>
-    <a @click="goToDetails">
-      <v-img
-          :src="posterUrl"
-      ></v-img>
-    </a>
-        <v-card-title>
-          {{title}}
-        </v-card-title>
-    <v-card-subtitle>
-      {{genre}}
-    </v-card-subtitle>
-    <v-card-text>
-      <v-row justify="center">
-        <v-rating
+  <v-hover v-slot="{ hover }">
+    <v-card>
+      <a @click="goToDetails">
+        <v-img :src="posterUrl"></v-img>
+      </a>
+      <v-card-title>
+        {{ title }}
+      </v-card-title>
+      <v-card-subtitle>
+        {{ genre }}
+      </v-card-subtitle>
+      <v-card-text>
+        <v-row justify="center">
+          <v-rating
             :value="parseFloat(rating)"
             background-color="white"
             color="orange"
@@ -21,52 +20,45 @@
             half-increments
             size="20"
             readonly
-        ></v-rating>
-      </v-row>
+          ></v-rating>
+        </v-row>
+      </v-card-text>
 
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        color="primary"
-        @click="goToDetails"
-      >
-        More
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        color="red"
-        @click="deleteMovie"
-      >
-        Delete
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-expand-transition>
+        <div v-show="hover">
+          <v-card-actions>
+            <v-btn text color="primary" @click="goToDetails"> More </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn text color="red" @click="deleteMovie"> Delete </v-btn>
+          </v-card-actions>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
 export default {
   name: "MovieCard",
-  props: ['id', 'title', 'posterUrl', 'genre', 'rating'],
+  props: ["id", "title", "posterUrl", "genre", "rating"],
   data() {
     return {
       show: false,
+      hover: true,
     };
   },
   methods: {
     // Changes URL to /movie/:id
     goToDetails() {
-      this.$router.push({path: `/movie/${this.id}`});
+      this.$router.push({ path: `/movie/${this.id}` });
     },
     // Sends ajax delete call to delete movie from database
     deleteMovie() {
-      this.$emit('show-dialog', this.id);
-    }
+      this.$emit("show-dialog", this.id);
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-
 </style>
